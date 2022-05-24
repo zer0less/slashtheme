c0a689622e36e250f99a0001785ffb4df8b3acfb
WithersWorld <asorinus@gmail.com>
2022-05-23 19:45:03 -0700

    v0.0.2

---
+++ color.js
@@ -0,0 +1,32 @@
+var themes = {
+    "super-dark-red": {
+        "black": "#000000",
+        "lightblack": "#1a1a1a",
+        "darklightred": "#b91212",
+        "darkred": "#860b04",
+        "red": "#FF1A0D",
+        "hotred": "#990404",
+        "darkorange": "#f06509",
+        "orange": "#FF8D00",
+        "yellow": "#FFD827",
+        "darkpurple": "#a80ffa",
+        "purple": "#B529FE",
+        "violet": "#eb07c5",
+        "lightpurple": "#E93BFF",
+    },
+
+    "super-light": {
+        "white": "#000000",
+        "darklightred": "#b91212",
+        "darkred": "#860b04",
+        "red": "#FF1A0D",
+        "hotred": "#990404",
+        "darkorange": "#f06509",
+        "orange": "#FF8D00",
+        "yellow": "#FFD827",
+        "darkpurple": "#a80ffa",
+        "purple": "#B529FE",
+        "violet": "#eb07c5",
+        "lightpurple": "#E93BFF",
+    }
+}
